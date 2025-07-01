11ff5b3047e4c437f3ceefca95f833cc384eef22
marcodev000 <marcosanchezdev@gmail.com>
2025-07-01 13:53:37 -0600

    Update header.php
    links actualizados

--- MyCMaquinados/almacen/templates/header.php
+++ MyCMaquinados/almacen/templates/header.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS primero -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+    <!-- JavaScript después, en ORDEN CORRECTO -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/build/toastr.min.js"></script>
 </head>
 
 <body>
@@ -21,7 +25,6 @@
             <a class="nav-item nav-link" href="solicitar_material.php">Solicitar Material</a>
             <a class="nav-item nav-link" href="gestionar_solicitudes.php">Solicitudes De Material</a>
             <a class="nav-item nav-link" href="surtido.php">Ingresar Material</a>
-       
             <a class="nav-item nav-link" href="logout.php">Cerrar Sesion</a>
         </div>
     </nav>
